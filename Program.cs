b6b7ee2578205303acd429f31911078cf2006a36
Haron-Terentin <haron.terentin@gmail.com>
2021-09-27 13:20:43 -0300

    Edição

--- Program.cs
+++ Program.cs
@@ -146,17 +146,19 @@ namespace DIO_Series
         private static string ObterOpcaoUsuario()
 		{
 			Console.WriteLine();
-			Console.WriteLine("DIO Séries a seu dispor!!!");
-			Console.WriteLine("Informe a opção desejada:");
+			Console.WriteLine("|---------------//------------|");
+			Console.WriteLine("|-DIO Séries a seu dispor!!!--|");
+			Console.WriteLine("|-Informe a opção desejada:---|");
+			Console.WriteLine("|                             |");
 
-			Console.WriteLine("1- Listar séries");
-			Console.WriteLine("2- Inserir nova série");
-			Console.WriteLine("3- Atualizar série");
-			Console.WriteLine("4- Excluir série");
-			Console.WriteLine("5- Visualizar série");
-			Console.WriteLine("C- Limpar Tela");
-			Console.WriteLine("X- Sair");
-			Console.WriteLine();
+			Console.WriteLine("|-----1- Listar séries--------|");
+			Console.WriteLine("|-----2- Inserir nova série---|");
+			Console.WriteLine("|-----3- Atualizar série------|");
+			Console.WriteLine("|-----4- Excluir série--------|");
+			Console.WriteLine("|-----5- Visualizar série-----|");
+			Console.WriteLine("|-----C- Limpar Tela----------|");
+			Console.WriteLine("|-----X- Sair-----------------|");
+			Console.WriteLine("|--------------//-------------|");
 
 			string opcaoUsuario = Console.ReadLine().ToUpper();
 			Console.WriteLine();
